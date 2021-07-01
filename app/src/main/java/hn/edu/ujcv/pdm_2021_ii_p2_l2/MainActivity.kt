91e387e4852f2c59839e491d6e46e69ae48ca586
Carlos <cmchamorro99@hotmail.com>
2021-07-01 17:09:12 -0600

    Trabajando en parte de matricula, esto solo es para pruebas

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_l2/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_l2/MainActivity.kt
@@ -1,5 +1,6 @@
 package hn.edu.ujcv.pdm_2021_ii_p2_l2
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import hn.edu.ujcv.pdm_2021_ii_p2_l2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_matricula.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    var alumno: HashMap<Int,String> = hashMapOf()
+    var clase: HashMap<Int,String> = hashMapOf()
+    var numero = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +39,23 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        button_first.setOnClickListener { irMatricula() }
+    }
+
+    fun irMatricula(){
+        alumno.put(numero++,"2017110190|Carlos Chamorro|carlos.chamorro@ujcv.edu.hn")
+        alumno.put(numero++,"2017130315|Angie Nicoll|cmchamorro5120@gmail.com")
+        numero = 1
+        clase.put(numero++,"FI1015|Filosofia|A|10:00 - 10:50|Academico 1-102")
+        clase.put(numero++,"ESP1014|Español|A|11:00 - 11:50|Academico 1-106")
+        clase.put(numero++,"MAT1014|Álgebra|B|09:00 - 09:50|Academico 3-300")
+        clase.put(numero++,"MAT1015|Geometría y Tri.|C|08:00 - 08:50|Academico 4-404")
+        clase.put(numero++,"IDI1015|Ingles I|A|07:00 - 07:50|Academico 2-102")
+        val intent = Intent(this, MatriculaActivity::class.java)
+        intent.putExtra("alumnos",alumno)
+        intent.putExtra("clases",clase)
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
