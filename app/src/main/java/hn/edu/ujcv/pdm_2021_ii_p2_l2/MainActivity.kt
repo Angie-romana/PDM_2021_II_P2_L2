20059023c1823cd424ab7cfe8e359b641c9c2256
Mitsuki <angienicoll7@gmail.com>
2021-07-05 13:04:01 -0600

    Cambios en fragment_first.xml FirstFragment.kt main main activity_main.xml

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_l2/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_l2/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var alumno: HashMap<Int,String> = hashMapOf()
     var clase: HashMap<Int,String> = hashMapOf()
+    var matricula: HashMap<Int,String> = hashMapOf()
     var numero = 1;
 
 
@@ -38,14 +39,12 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
 
-        button_first.setOnClickListener { irMatricula() }
-        button.setOnClickListener { irClase() }
-        button3.setOnClickListener { irAlumno() }
+
+        button_inicio.setOnClickListener {  }
+
+        button_inicio.setOnClickListener { irAlumno() }
+
 
     }
 
@@ -62,17 +61,21 @@ class MainActivity : AppCompatActivity() {
 
 
     fun irMatricula(){
-        alumno.put(numero++,"2017110190|Carlos Chamorro|carlos.chamorro@ujcv.edu.hn")
-        alumno.put(numero++,"2017130315|Angie Nicoll|cmchamorro5120@gmail.com")
+        alumno.put(numero++,"2017110190|Carlos Chamorro|angienicoll7@gmail.com")
+        alumno.put(numero++,"2017130315|Angie Nicoll|angienicoll7@gmail.com")
         numero = 1
         clase.put(numero++,"FIL1015|Filosofia|A|10:00 - 10:50|Academico|1|102")
         clase.put(numero++,"ESP1014|Español|A|11:00 - 11:50|Academico|1|106")
         clase.put(numero++,"MAT1014|Álgebra|B|09:00 - 09:50|Academico|3|300")
         clase.put(numero++,"MAT1015|Geometría y Tri.|C|08:00 - 08:50|Academico|4|404")
         clase.put(numero++,"IDI1015|Ingles I|A|07:00 - 07:50|Academico|2|202")
+
+        /*matricula.put(numero++,"2017110190|MAT1015")
+        matricula.put(numero++,"2017130315|IDI1015")*/
         val intent = Intent(this, MatriculaActivity::class.java)
         intent.putExtra("alumnos",alumno)
         intent.putExtra("clases",clase)
+       // intent.putExtra("matriculas",matricula)
         startActivity(intent)
     }
 
